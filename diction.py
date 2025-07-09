d855ddf1aa757533b96c522e8039acf6355896fa
JonathanK-N <101374195+JonathanK-N@users.noreply.github.com>
2025-07-09 12:56:25 -0400

    Use flake8 config in CI

--- diction.py
+++ diction.py
@@ -3,10 +3,10 @@ import pyttsx3
 import json
 import speech_recognition as sr
 
-data = json.load(open('data.json'))
+data = json.load(open("data.json"))
 engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
 
 
 def speak(audio):
@@ -17,22 +17,22 @@ def speak(audio):
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print('Listening...')
+        print("Listening...")
         r.pause_threshold = 1
         r.energy_threshold = 494
         r.adjust_for_ambient_noise(source, duration=1.5)
         audio = r.listen(source)
 
     try:
-        print('Recognizing..')
-        query = r.recognize_google(audio, language='en-in')
-        print(f'User said: {query}\n')
+        print("Recognizing..")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said: {query}\n")
 
-    except Exception as e:
-        # print(e)
+    except Exception:
+        # print(Exception)
 
-        print('Say that again please...')
-        return 'None'
+        print("Say that again please...")
+        return "None"
     return query
 
 
@@ -42,20 +42,19 @@ def translate(word):
         speak(data[word])
     elif len(get_close_matches(word, data.keys())) > 0:
         x = get_close_matches(word, data.keys())[0]
-        speak('Did you mean ' + x +
-              ' instead,  respond with Yes or No.')
+        speak("Did you mean " + x + " instead,  respond with Yes or No.")
         ans = takeCommand().lower()
-        if 'yes' in ans:
+        if "yes" in ans:
             speak(data[x])
-        elif 'no' in ans:
+        elif "no" in ans:
             speak("Word doesn't exist. Please make sure you spelled it correctly.")
         else:
-            #changed from we to I
+            # changed from we to I
             speak("I did not understand your entry.")
 
     else:
         speak("Word doesn't exist. Please double check it.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     translate()
